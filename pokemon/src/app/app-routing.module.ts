e189ec2aa5a37e21d431f349aef199fef252a4b4
khaled-elshahat-prove <elshahatkhaled43@gmail.com>
2022-10-27 18:12:15 +0000

    punto 2

--- pokemon/src/app/app-routing.module.ts
+++ pokemon/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemPokemonComponent } from './item-pokemon/item-pokemon.component';
+import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search-pokemon', component: SearchPokemonComponent },
+  { path: 'item-pokemon/:id', component: ItemPokemonComponent },
+  { path: '', redirectTo: '/search-pokemon', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pokemon/src/app/app.module.ts
+++ pokemon/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
+import { ItemPokemonComponent } from './item-pokemon/item-pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPokemonComponent,
+    ItemPokemonComponent
   ],
   imports: [
     BrowserModule,
